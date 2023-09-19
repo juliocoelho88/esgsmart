928461b76ab06e5bb560704a588f73152fa6e0a1
julio <julio.alemao8825@gmail.com>
2023-09-19 10:48:39 -0300

    Exportado do springinit.

---
+++ src/main/java/br/com/fiap/esgsmart/EsgsmartApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.esgsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgsmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgsmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/esgsmart/EsgsmartApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.esgsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgsmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
