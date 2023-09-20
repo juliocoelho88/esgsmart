0d0851dfa2a1db456aef4c567caf09d2fddba342
Vinicius Vieira <viniciusvsoares00@gmail.com>
2023-09-20 11:32:27 -0300

    adicionando o controller DispositivoIotResource

--- src/main/java/br/com/fiap/esgsmart/controller/CidadaoResource.java
+++ src/main/java/br/com/fiap/esgsmart/controller/CidadaoResource.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("cidadao")
 public class CidadaoResource {
+
     @Autowired
     private CidadaoRepository cidadaoRepository;
 
@@ -18,20 +19,24 @@ public class CidadaoResource {
     public List<Cidadao> listar(){
         return cidadaoRepository.findAll();
     }
+
     @GetMapping("{codigo}")
     public Cidadao buscar(@PathVariable long codigo){
         return cidadaoRepository.findById(codigo).get();
     }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Cidadao cadastrar(@RequestBody Cidadao cidadao){
         return cidadaoRepository.save(cidadao);
     }
+
     @PutMapping("{id}")
     public Cidadao atualizar(@RequestBody Cidadao cidadao, @PathVariable long id){
         cidadao.setId(id);
         return cidadaoRepository.save(cidadao);
     }
+
     @DeleteMapping("{codigo}")
     public void remover(@PathVariable long codigo){
         cidadaoRepository.deleteById(codigo);
