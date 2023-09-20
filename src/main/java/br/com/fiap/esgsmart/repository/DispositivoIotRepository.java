eb63cab22d1e8aa4291b3ca3a06f03f0a52feab9
Vinicius Vieira <viniciusvsoares00@gmail.com>
2023-09-20 10:33:48 -0300

    adicionando controller

--- src/main/java/br/com/fiap/esgsmart/repository/DispositivoIotRepository.java
+++ src/main/java/br/com/fiap/esgsmart/repository/DispositivoIotRepository.java
@@ -1,8 +1,6 @@
 package br.com.fiap.esgsmart.repository;
 
 import br.com.fiap.esgsmart.model.DispositivoIoT;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
